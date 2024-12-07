02890bd6140d90b69890e40730799b2e6d639847
GinnLee <ginn0723@icloud.com>
2024-12-08 03:14:33 +0800

    fix update

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -27,17 +27,12 @@ VALUES
 
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 
-UPDATE
-  "USER"
-SET
-  "role" = 'COACH'
-WHERE
-  "email" IN (
-    'lee2000@hexschooltest.io',
-    'muscle@hexschooltest.io',
-    'starplatinum@hexschooltest.io'
-  )
-  AND "role" = 'USER';
+-- Update the role for the given users
+UPDATE "USER"
+SET "role" = 'COACH'
+WHERE "email" IN ('lee2000@hexschooltest.io', 'muscle@hexschooltest.io', 'starplatinum@hexschooltest.io')
+AND "role" = 'USER';
+
 
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
 
